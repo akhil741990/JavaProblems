fc69ba502c4a8703ccf26c9f8ce86dd51337ae1c
Akhil Vishwanathan Pillai <akhil741990@gmail.com>
2018-10-07 16:26:09 +0530

    Added array max diff finder

---
+++ src/problem/hackerrank/ArrayMaxDiffFinder.java
@@ -0,0 +1,26 @@
+package problem.hackerrank;
+
+public class ArrayMaxDiffFinder {
+	public static void main(String args[]){
+		int a[] = {26,8,7,6,4};
+		System.out.println(maxDiffFinder(a));
+	}
+	
+	public static int maxDiffFinder(int a[]){
+		int maxDiff = a[1] - a[0];
+		int min = a[0];
+		for (int i=1;i<a.length;i++){
+			if((a[i] - min) > maxDiff){
+				maxDiff = a[i] - min;
+			}
+			if(a[i]<min){
+				min = a[i];
+			}
+		}
+		return maxDiff; 
+	}
+	
+	public static void test1(int arr[]){
+		
+	}
+}
