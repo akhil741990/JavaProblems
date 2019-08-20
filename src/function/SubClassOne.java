3123bde5d96c0bc8f6715b8cccca53ee863d3e87
Akhil Pillai <akhil741990@gmail.com>
2019-08-20 10:16:00 +0530

    converted to maven project

---
+++ src/function/SubClassOne.java
@@ -0,0 +1,13 @@
+package function;
+
+import java.util.function.Function;
+
+public class SubClassOne extends MyUtility implements Function<Object, Object>{
+
+	@Override
+	public Object apply(Object t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/function/Test.java
@@ -0,0 +1,11 @@
+package function;
+
+import java.util.function.Function;
+
+public class Test {
+
+	public static void main(String args[]){
+		
+		Function<Object, Object> func = new SubClassOne();
+	}
+}
